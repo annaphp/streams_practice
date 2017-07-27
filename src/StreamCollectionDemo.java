9d9f19b26e7300bc1592cd42d3366dda7362b1f4
Anna Ilchenko <mymihtav@gmail.com>
2017-07-26 22:52:03 -0700

    completed exercise 5

--- src/StreamCollectionDemo.java
+++ src/StreamCollectionDemo.java
@@ -4,9 +4,13 @@ public class StreamCollectionDemo {
 
 	public static void main(String[] args) {
 		
-	 int[] numbers = {1,2,-4,5};
-	 printDoubled(numbers);
+	 int[] numbers = {1,2,-4,5,80};
+	 double[] radii = {3.0,1.0,7.2,5.5}; 
+//	 printDoubled(numbers);
 	 System.out.println(sumAbVals(numbers));
+	 System.out.println(largestEven(numbers));
+	 System.out.println(totalCircleArea(radii));
+	 System.out.println(countNegatives(numbers));
 
 	}
 	
@@ -18,4 +22,15 @@ public class StreamCollectionDemo {
 		return Arrays.stream(numbers).reduce(0, (a, b) -> Math.abs(a)+Math.abs(b));
 	}
 
+	public static int largestEven(int[] numbers){
+		return Arrays.stream(numbers).filter(n -> n % 2 == 0).max().getAsInt();
+	}
+	
+	public static double totalCircleArea(double[] numbers){
+		return Arrays.stream(numbers).map(n -> Math.round(Math.PI * n * n)).sum();
+	}
+	
+	public static long countNegatives(int[] numbers){
+		return Arrays.stream(numbers).filter(n -> n < 0).count();
+	}
 }
